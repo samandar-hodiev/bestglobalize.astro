e643ef7481f41d6fa8a76a912c84624ba8a6a690
samandar hodiev <samandarhodiev@MacBook-Pro.local>
2025-03-09 05:52:53 +0500

    RWD doing...

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,46 +1,52 @@
 export const servicesData = [
-    {
-        title: "Hujjatlarni ishlab chiqish",
-        icon: "bi bi-file-earmark-word",
-        description: "Biz barcha hujjatlarni rasmiylashtirish bilan shug'ullanamiz. Inkassatsiyadan tortib rasmiylashtirishgacha, bizga ishonib topshiring, shunda siz muhim narsalarga e'tibor qaratishingiz mumkin."
-    },
-    {
-        title: "Hujjatlarni tezkor qayta ishlash",
-        icon: "bi bi-clock-history",
-        description: "Tez hujjatlarni rasmiylashtirish bizning ixtisosimiz. Biz kerakli hujjatlarni yig'ish va tayyorlashda samaradorlik va aniqlikni kafolatlaymiz. Ushbu jarayonni bizga ishoning va keraksiz qiyinchiliklardan xalos bo'ling."
-    },
-    {
-        title: "Ish beruvchilardan bevosita bo'sh ish o'rinlari",
-        icon: "bi bi-person-workspace",
-        description: "Bizda faqat ish beruvchilardan to'g'ridan-to'g'ri bo'sh ish o'rinlari mavjud. Hech qanday vositachi yo'q, faqat sizning ish joyingiz uchun to'g'ridan-to'g'ri imkoniyatlar."
-    },
+  {
+    title: `Hujjat tayyorlash`,
+    icon: `bi bi-file-earmark-word`,
+    description: `Biz barcha hujjatlarni rasmiylashtirish bilan shug ullanamiz.
+Inkassatsiyadan tortib rasmiylashtirishgacha, bizga hujjatlarni
+rasmiylashtirishga ishoning, shunda diqgatingizni muhim
+narsalarga garating.`,
+  },
+  {
+    title: "Hujjatlarni tezkor qayta ishlash",
+    icon: "bi bi-clock-history",
+    description: `Tez hujjatlarni rasmiylashtirish bizning ixtisosimiz. Biz kerakli
+hujjatlarni yig'ish va tayyorlashda samaradorlik va aniqlikni
+kafolatlaymiz. Ushbu jarayonni bizga ishoning va keraksiz
+giyinchiliklardan xalos boling.`,
+  },
+  {
+    title: "Ish beruvchilardan bevosita bo'sh ish o'rinlari",
+    icon: "bi bi-person-workspace",
+    description:`Bizda faqat ish beruvchilardan to gridan-to gri bo'sh ish o rinlari mavjud. Hech ganday vositachi yo'q, faqat sizning ish joyingiz uchun to gridan-to gri imkoniyatlar.`
+  },
 
-    {
-        title: "Viza kafolati",
-        icon: "bi bi-file-earmark-break",
-        description: "Naimix bilan Work and Travel orzularingizni amalga oshiring! Biz nafaqat viza olish uchun 92% kafolat beramiz, balki chet elda ishlash va sayohat qilish rejalaringizni amalga oshirishda ham yordam beramiz. Jonli va unutilmas Work and Travel tajribasi uchun Naimix tajribasi va yordamiga ishoning."
-    }
+  {
+    title: "Viza olishda ko'maklashamiz",
+    icon: "bi bi-file-earmark-break",
+    description:`Bestglobalize bilan Work and Travel orzularingizni amalga oshiring!
+Biz nafaqat viza olish uchun yordam beramiz, balki chet elga ishlash va sayohat gilish rejalaringizni amalga shirishga ham yordam beramiz. Ushbu jarayonda giyinchiliklardan xalos boling.`
+  },
 ];
 
-
 export const information = [
-    {
-        icon: 'bi bi-headset',
-        info: " Onlayn maslahat Sizning qulayligingiz uchun",
-    },
+  {
+    icon: "bi bi-headset",
+    info: " Onlayn maslahat Sizning qulayligingiz uchun",
+  },
 
-    {
-        icon: 'bi bi-microsoft-teams',
-        info: "Tajribali Mutaxassislar Keng ish tajribasi",
-    },
+  {
+    icon: "bi bi-microsoft-teams",
+    info: "Tajribali Mutaxassislar Keng ish tajribasi",
+  },
 
-    {
-        icon: 'bi bi-pc-display',
-        info: "Diversifikatsiyalangan xizmatlar Sizning qulayligingiz uchun",
-    },
+  {
+    icon: "bi bi-pc-display",
+    info: "Diversifikatsiyalangan xizmatlar Sizning qulayligingiz uchun",
+  },
 
-    {
-        icon: 'bi bi-file-earmark-pdf',
-        info: "Yuqori sifatli ishlash Kerakli hujjatlarni tayyorlash",
-    },
+  {
+    icon: "bi bi-file-earmark-pdf",
+    info: "Yuqori sifatli ishlash Kerakli hujjatlarni tayyorlash",
+  },
 ];
